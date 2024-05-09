971bf10984f42d6a1982f2de36cec4b4c0e2d4f2
Mayank Mishra <89137604+mmayank591@users.noreply.github.com>
2024-05-09 17:20:37 +0530

    created signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+                                                    //salt
+  const hashedPassword = bcryptjs.hashSync(password, 10); //hashsyc isliye use kiye kyko await use na karna pade
+  const newUser = new User({ username, email, password: hashedPassword });
+try {
+  await newUser.save(); //it can take time therefore we have used await so that the code stays in this line until this operation finishes
+  res.status(201).json("User created successfully!");
+} catch (error) {
+  res.status(500).json(error.errorResponse.errmsg);
+}
+
+ 
+};
--- api/index.js
+++ api/index.js
@@ -2,8 +2,10 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from'./routes/user.route.js';
+import authRouter from './routes/auth.route.js'
 dotenv.config();
 const app = express();
+app.use((express.json()));
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -19,3 +21,4 @@ app.listen(3000, () => {
 
 //routes
 app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
