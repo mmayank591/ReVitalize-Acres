402acb47e592ae19f14ee7b2646a6d2fdd025be9
Mayank Mishra <89137604+mmayank591@users.noreply.github.com>
2024-05-11 22:37:07 +0530

    add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {store}  from './redux/store.js';
+import { persistor,store}  from './redux/store.js';//persistor: This is an instance of the Redux persist Persistor object. It's responsible for persisting and rehydrating the Redux store's state according to the configuration set in persistConfig
 import {Provider} from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';//PersistGate: This is a React component provided by redux-persist/integration/react. It's used to delay the rendering of your application's UI until the persisted state has been retrieved and rehydrated. 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
+    <PersistGate  loading={null} persistor={persistor}>
     <App />
+    </PersistGate>
   </Provider>
 )
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,24 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from './user/userSlice';
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import userReducer from "./user/userSlice";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import persistStore from "redux-persist/es/persistStore";
+const rootReducer = combineReducers({ user: userReducer });
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer); //persistReducer: This function is provided by redux-persist. It takes two arguments:
+// persistConfig: This is the configuration object that specifies how and where the state should be persisted. It includes options such as storage (localStorage, AsyncStorage, etc.), whitelist/blacklist of reducers, and serialization options.
+// rootReducer: This is the root reducer of your Redux application. It's the top-level reducer that combines all other reducers into a single state tree.
+// persistedReducer: This variable holds the enhanced reducer returned by persistReducer. The enhanced reducer automatically handles persisting the state based on the configuration provided.
 
 export const store = configureStore({
-  reducer: {user:userReducer},
-  middleware:(getDefaultMiddleware)=>getDefaultMiddleware({
-  serializableCheck:false,
-  }),
-
-})
+  reducer:persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+export const persistor = persistStore(store); //this persistoor will amke the store persist
