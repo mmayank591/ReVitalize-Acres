af0c3f060fa2a8ea8821389237100086ebd58494
Mayank Mishra <89137604+mmayank591@users.noreply.github.com>
2024-06-25 17:23:00 +0530

    complete about page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,8 +27,8 @@ export default function Header() {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold  text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-500 ">ReVitalize</span>
-            <span className="text-slate-700">Acres</span>
+            <span className="text-slate-500 ">MakaanMitra</span>
+            <span className="text-slate-700">.Com</span>
           </h1>
         </Link>
         <form onSubmit={handleSubmit} className="bg-slate-100 p-3 rounded-lg flex items-center">
